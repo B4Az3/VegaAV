c973084763509aaaac8e452e3f13c2156f33715d
B4Az3 <cagliyanalisadi@gmail.com>
2025-02-19 23:01:25 +0300

    Main files
    might be able to port it on linux/GNU systems

---
+++ setup.bat
@@ -0,0 +1,9 @@
+winget install python
+pip install --upgrade pip
+pip install pyfiglet
+pip install Pillow
+pip install libmagic --user
+pip install --upgrade libmagic
+pip install python-magic-bin==0.4.14
+pip install hashlib
+pip install requests
